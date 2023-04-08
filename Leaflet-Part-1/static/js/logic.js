3ba76aa6a61e92b9119b06552043f485bc33589c
drainlm <119275749+drainlm@users.noreply.github.com>
2023-04-08 10:52:20 -0500

    final commit

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -1,48 +1,76 @@
 function createMap(earthquakeMarkers) {
     // Create the tile layer that will be the background of our map.
-    let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    let map = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
-  
-    // Create a baseMaps object to hold the streetmap layer.
-    let baseMaps = {
-      "Street Map": streetmap
-    };
-  
+
     // Create an overlayMaps object to hold the Earthquakes layer.
     let overlayMaps = {
       "Earthquakes": earthquakeMarkers
     };
   
     // Create the map object with options.
-    let map = L.map("map", {
-      center: [37.7749, -122.4194],
+    let myMap = L.map("map", {
+      center: [39.7392, -104.9903],
       zoom: 5,
-      layers: [streetmap, earthquakeMarkers]
+      layers: [map, earthquakeMarkers]
     });
 
-    // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
-    L.control.layers(baseMaps, overlayMaps, {
-        collapsed: false
-      }).addTo(map);
+    // Set up the legend.
+    let legend = L.control({ position: "bottomright" });
+    legend.onAdd = function() {
+        let div = L.DomUtil.create("div", "info legend");
+        let limits = [-10,10,30,50,70,90];
+        let colors = ['#B6F34C', '#E1F34C', '#F3DB4C', '#F3B94C', '#F0A76A','#F06A6A'];
+    
+        // Create the legend content with the limits and colors.
+        for (let i = 0; i < limits.length; i++) {
+            div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
+                + limits[i] + (limits[i + 1] ? "&ndash;" + limits[i + 1] + "<br>" : "+");
+        }
+        return div;
+    };
+  
+  // Adding the legend to the map
+  legend.addTo(myMap);
+
     }
 
-function createMarkers(response) {
+// createMarkers Function 
 
+function createMarkers(response) {
     // Pull the "earthquakes" property from response.features.
     let earthquakes = response.features;
 
     // Initialize an array to hold earthquake markers.
     let earthquakeMarkers = [];
 
+    // Function to return depth value color to markers
+    function getValue(x) {
+        return x > 90 ? "#F06A6A" :
+               x > 70 ? "#F0A76A" :
+               x > 50 ? "#F3B94C" :
+               x > 30 ? "#F3DB4C" :
+               x > 10 ? "#E1F34C" :
+                   "#B6F34C";
+    }
+
     // Loop through the earthquakes array.
     for (let index = 0; index < earthquakes.length; index++) {
-    let earthquake = earthquakes[index];
+        let earthquake = earthquakes[index];
 
-    // **For each station, create a marker, and bind a popup with the station's name.
+    // For each earthquake, create a marker, and bind a popup with the station's name.
     let earthquakeMarker = L.circleMarker([earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]], {
-      }).bindPopup("<h3>Where: " + earthquake.properties.place +
-      "<h3><h3>When: " + new Date(earthquake.properties.time) + "<h3><h3>What: " + earthquake.properties.mag + " " + earthquake.properties.type + "</h3>");
+        fillOpacity: .90,
+        weight: 1,
+        color: "black",
+        fillColor: getValue(earthquake.geometry.coordinates[2]),
+        radius: (earthquake.properties.mag * 4)
+      }).bindPopup("<h3><h3>A " + earthquake.properties.mag + " magnitude " + earthquake.properties.type + 
+      "<h3> with a depth of " + earthquake.geometry.coordinates[1] + "km occurred</h3>" +
+        "<h3>" + earthquake.properties.place +
+      "<h3><h3>on " + new Date(earthquake.properties.time)  
+      );
     
     // Add the marker to the earthquakeMarker array.
     earthquakeMarkers.push(earthquakeMarker);
--- README.md
+++ README.md
@@ -2,37 +2,17 @@
 
 ### Earthquake Visualization
 
-This code uses Leaflet.js and [USGS GeoJSON Feed](http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php) to create an interactive map displaying earthquake data from the past 7 days. The map uses *clustered* markers with popups to share information about the location, date and time, magnitude, and type (earthquake or quarry). *The markers also reflect the magnitude by their size and depth by color. A legend is included to provide context.*
+This code uses Leaflet.js and [USGS GeoJSON Feed](http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php) to create an interactive map displaying earthquake data from the past 7 days. The map uses markers with popups to share information about the location, date and time, magnitude, and type (earthquake or quarry). The markers also reflect the magnitude by their size and depth by color. A legend is included to provide context.
 
-`<png>`
+![1680930965572](image/README/1680930965572.png)
 
-Functions: 
+Functions:
 
-1. The `<createMap>` function uses the openstreetmap as a base layer and overlays with the earthquake markers array.
-2. The `<createMarkers>` function processes the earthquake data as a response object and creates a circle marker using the earthquake coordinates. It then binds a popup including the earthquake's place, date and time, magnitude, and type. 
+1. The `<createMap>` function uses the openstreetmap as a base layer and overlays with the earthquake markers array. A legend is then added to the bottom right of the screen to provide context to the colored markers indicating the depth of the earthquake.
+
+   ![1680930939382](image/README/1680930939382.png)
+2. The `<createMarkers>` function processes the earthquake data as a response object and creates a circle marker using the earthquake coordinates. It then binds a popup including the magnitude, type, depth, place, and date and time of occurrance. A `<getValue>` function returns an associated color for the depth value to be used for the marker.
+
+![1680930895956](image/README/1680930895956.png)
 
 An API call to the USGS to get the information is then made, which then calls to `<createMarkers>` when it completes.
-
-    ![2-BasicMap](https://static.bc-edx.com/data/dl-1-2/m15/lms/img/2-BasicMap.jpg)
-
-
-### Part 2: Gather and Plot More Data (Optional with no extra points earning)
-
-    Plot a second dataset on your map to illustrate the relationship
-between tectonic plates and seismic activity. You will need to pull in
-this dataset and visualize it alongside your original data. Data on
-tectonic plates can be found at[https://github.com/fraxen/tectonicplates](https://github.com/fraxen/tectonicplates)
-
-    This part is completely optional; you can complete this part as a way to challenge yourself and boost your new skills.
-
-    The following image is an example screenshot of what you should produce:
-
-    ![5-Advanced](https://static.bc-edx.com/data/dl-1-2/m15/lms/img/5-Advanced.jpg)
-
-    Perform the following tasks:
-
-    * Plot the tectonic plates dataset on the map in addition to the earthquakes.
-
-* Add other base maps to choose from.
-* Put each dataset into separate overlays that can be turned on and off independently.
-* Add layer controls to your map.
